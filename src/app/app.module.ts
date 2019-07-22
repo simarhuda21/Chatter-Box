50221004354d1f0e92285e6bb2656cce7fee6a3f
simarhuda21 <simarsingh.huda.sa@gmail.com>
2019-07-22 19:53:21 +0530

    adding new componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+
 
 import { AppComponent } from './app.component';
+import { ActiveListComponent } from './components/active-list/active-list.component';
+import { ChatRoomComponent } from './components/chat-room/chat-room.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MessageComponent } from './components/message/message.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FlashMessagesModule} from 'angular2-flash-messages';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActiveListComponent,
+    ChatRoomComponent,
+    HomeComponent,
+    LoginComponent,
+    MessageComponent,
+    NavbarComponent,
+    ProfileComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FlashMessagesModule.forRoot(),
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/active-list/active-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-active-list',
+  templateUrl: './active-list.component.html',
+  styleUrls: ['./active-list.component.scss']
+})
+export class ActiveListComponent implements OnInit {
+  @Input() users: Array<string>;
+  @Input() current: string;
+  @Output() newConv = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onUserClick(username: string): boolean {
+    this.newConv.emit(username);
+    return false;
+  }
+
+}
